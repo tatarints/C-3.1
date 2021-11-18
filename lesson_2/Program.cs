3e0ca128429f62649052f936126b9b3aefb2f985
tatarints <tvlt2512@gmail.com>
2021-11-18 20:01:24 +0500

    Добавьте файлы проекта.

---
+++ lesson_2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+
+            Console.WriteLine("Введите тип банковского счёта: ");
+            bank.TypeAccount = Console.ReadLine();
+            
+            Bank bank1 = new Bank(1);
+
+            Console.WriteLine("Номер счёта " + bank.AccountNumber +
+                    ", тип банковского счёта: " + bank.TypeAccount +
+                    ", баланс: " + bank1.Balance + ".");
+            Console.ReadKey();
+        }
+    }
+
+    public class Bank
+    {
+        public int AccountNumber { get; set; }
+        public int Balance { get; set; }
+        public string TypeAccount { get; set; }
+
+
+        public Bank()
+        {
+            Console.WriteLine("Номер счета: ");
+            Random rnd = new Random();
+            AccountNumber = rnd.Next(1000000000, int.MaxValue);
+            Console.WriteLine(AccountNumber);
+        }
+        public Bank(int balance)
+        {
+            Console.WriteLine("Введите количество денег на счету: ");
+            Balance = int.Parse(Console.ReadLine());
+        }
+        public Bank(string typeaccount)
+        {
+
+        }
+    }
+}
